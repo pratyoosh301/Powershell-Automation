b0d64e9d4adfb9efcf4da3c363792e115f7e413b
pratyoosh301 <pratyooshmishra2000@gmail.com>
2025-07-07 21:05:21 +0000

    initial commit

---
+++ disk2.ps1
@@ -0,0 +1,9 @@
+$threshold=10
+$free=[Math]::round(((Get-PsDrive -Name "C").Free/1GB),2)
+$total=(Get-PsDrive -Name "C").Used/1GB+$free
+$freepercent=($free/$total)*100
+if($freepercent -lt $threshold){
+Write-Output "below Threshhold its $freepercent"
+}
+else{
+Write-Output "its safe its $free and total is $total"}
